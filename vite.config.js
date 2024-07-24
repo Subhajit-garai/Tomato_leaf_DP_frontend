61828558c4ba95e64b5241f86b6aaf03254148a9
subhajit60 <subhajitgarai988@gmail.com>
2024-07-24 15:18:28 +0530

    add render link

--- vite.config.js
+++ vite.config.js
@@ -8,14 +8,16 @@ export default defineConfig({
       plugins: ["babel-plugin-styled-components"]
     }
   })],
+
   server: {
     proxy: {
       '/api': {
-        target: 'http://13.235.209.201:4000', // Target backend server
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, '/api/v1') // Optional: Rewrite the API path if needed
+        target:'https://tldp-node-server-0-0-1.onrender.com',
+        changeOrigin:true,
+        secure: false,
       }
-    }
+       
+        }
   },
 
 })
